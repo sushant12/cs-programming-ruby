b1b960be95e30fdbb8c6a473c8b8390f125191e3
sushant12 <sushatnbajracharya@gmail.com>
2015-12-10 19:04:51 +0545

    updated chap4 ex1 and ex2

---
+++ chap4/compare.rb
@@ -0,0 +1,37 @@
+## ex1
+#puts("Enter an integer: ")
+#num = gets.to_i.ceil
+#if num%2 == 0
+#	"Even"
+#else
+# "Odd"
+#end
+
+##ex2a
+#puts ("Enter number between 5-10")
+#num =  gets.to_i
+#if num == 5..10 
+#p "correct"
+#else
+# p "incorrect"
+#end
+
+##ex2b
+#puts ("Enter number between 5-10")
+#num = gets.to_i
+#case num
+#when num < 5 then p "less than 5"
+#when num > 10 then p "greater than 10"
+#when num == 5 || num == 6 || num == 7 || num == 8 || num == 9 || num == 10 then "in the range"
+#when 5..10 then p "in the range"
+#else
+#p "no inpout"
+#end
+
+
+##ex3
+puts ("Enter x value")
+x = gets.to_i
+puts ("Enter y value")
+#y = gets.to_i
+
