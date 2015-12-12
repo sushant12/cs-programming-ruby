274fec40ef152054797d77cdc18c879f50f98c28
sushant12 <sushatnbajracharya@gmail.com>
2015-12-12 08:08:43 +0545

    finished chap4 ex4

--- chap4/4.rb
+++ chap4/4.rb
@@ -1,1 +1,23 @@
 #elimination mehtod and substitution method
+puts "Please enter the slope for the first line (m):" 
+m_1 = gets.to_f
+
+puts "Please enter the y-intercept for the first line (b):"
+b_1 = gets.to_f
+
+puts "Please enter the slope for the second line (m):" 
+m_2 = gets.to_f
+
+puts "Please enter the y-intercept for the second line (b):"
+b_2 = gets.to_f
+
+solution = (b_1 - b_2) / (m_1 - m_2)
+
+case
+    when ((b_1 == b_2) and (m_1 == m_2))
+    puts "The lines are the same! There are infinitely many solutions."
+    when ((m_1 == m_2) and (b_1 != b_2))
+    puts "No solutions, the lines are parallel." 
+    else 
+    puts solution
+end   
