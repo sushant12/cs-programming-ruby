aefac228abdbaa25a55e6c58f05372a83d8fd05d
sushant12 <sushatnbajracharya@gmail.com>
2015-12-17 19:17:26 +0545

    finished chap5 ex4

--- chap5/4.rb
+++ chap5/4.rb
@@ -4,3 +4,18 @@ puts "Enter a number"
 num = gets.chomp.to_f
 puts "Enter divisor"
 divisor = gets.chomp.to_f
+
+multiples = []
+
+found = false
+i = 1
+until found == true
+	if divisor*i <= num
+		multiples << divisor*i
+	else
+		found = true
+	end	
+	i += 1
+end
+
+p num-multiples.last
